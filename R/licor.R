5cb47a4bb972dfd73d695f37997ea40ad2072bbf
unknown <rsimon@wxp-rsimon.CIP.CGIARAD.ORG>
2013-09-04 00:25:10 -0500

    Refactoring code.

--- R/licor.R
+++ R/licor.R
@@ -1,3 +1,31 @@
+# convert to zero or one
+convertVals = function(vals, refs){
+  d3 = refs %in% vals
+  d3 = as.numeric(d3) 
+  d3
+}
+
+#identify all unique values ignoring -1 and -9 (or anything below 0)
+identifyUniqueVals <- function (data) {
+  d1= data[,c(3:ncol(data))]
+  d2=sort(unique(unlist(mapply(unique, d1))))
+  d2=d2[d2>0]
+  d2
+}
+
+createMatrix <- function (data, d2) {
+  #create new matrix
+  res = t(apply(data[,3:ncol(data)], 1, convertVals, d2 )) 
+  res = as.data.frame(res)
+  
+  #label columns and rows
+  res = cbind(data[,1], res)
+  names(res)=c("Genotype",paste(unique(data[,2]),d2,sep="."))
+  res
+}
+
+
+
 #' Convert licor data into matrix of zero and one
 #' 
 #' Assumes a bp weight value or -1 and -9; the latter will be replaced by 0; the weight by 1.
@@ -18,24 +46,8 @@ licor2matrix <- function(filename=NULL){
   if(length(filename)>0){
     data = read.csv(filename, stringsAsFactors=FALSE, header=F, sep="\t")
     
-    #identify all unique values ignoring -1 and -9 (or anything below 0)
-    d1= data[,c(3:ncol(data))]
-    d2=sort(unique(unlist(mapply(unique, d1))))
-    d2=d2[d2>0]
-    
-    #convert to zero or one
-    convertVals = function(vals, refs){
-      d3 = refs %in% vals
-      d3 = as.numeric(d3) 
-      d3
-    }
-    #create new matrix
-    res = t(apply(data[,3:ncol(data)], 1, convertVals, d2 ))
-    res = as.data.frame(res)
-    
-    #label columns and rows
-    res = cbind(data[,1], res)
-    names(res)=c("Genotype",paste(unique(data[,2]),d2,sep="."))
+    d2 = identifyUniqueVals(data)
+    res= createMatrix(data, d2)
     
     #store
     outname = file.path(dirname(filename), gsub(".txt","_out.csv",basename(filename),) )
