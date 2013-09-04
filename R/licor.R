463bb30a4a6f4d19dc9c77d866f9b3dafd60f7e6
unknown <rsimon@wxp-rsimon.CIP.CGIARAD.ORG>
2013-09-03 23:26:21 -0500

    First function to convert licor data.

---
+++ R/licor.R
@@ -0,0 +1,49 @@
+#' Convert licor data into matrix of zero and one
+#' 
+#' Assumes a bp weight value or -1 and -9; the latter will be replaced by 0; the weight by 1.
+#' The column will be renamed with the bp weight. The function also creates a file of the 
+#' transformed data in .csv format within the the same path. The file name is appended with '_out'
+#' and the extension set to '.csv'.
+#' 
+#' @param filename an optional filename given the full path or relative path. If not given the user is asked.
+#' @return transformed data if succesful; NA if otherwise
+#' @author Reinhard Simon
+#' @export
+licor2matrix <- function(filename=NULL){
+  if(is.null(filename)) {
+    filename = choose.files(default = "", caption = "Select licor file!",
+                            filters = c("Licor data","*.txt") )
+  } 
+  res=NA
+  if(length(filename)>0){
+    data = read.csv(filename, stringsAsFactors=FALSE, header=F, sep="\t")
+    
+    #identify all unique values ignoring -1 and -9 (or anything below 0)
+    d1= data[,c(3:ncol(data))]
+    d2=sort(unique(unlist(mapply(unique, d1))))
+    d2=d2[d2>0]
+    
+    #convert to zero or one
+    convertVals = function(vals, refs){
+      d3 = refs %in% vals
+      d3 = as.numeric(d3) 
+      d3
+    }
+    #create new matrix
+    res = t(apply(data[,3:ncol(data)], 1, convertVals, d2 ))
+    res = as.data.frame(res)
+    
+    #label columns and rows
+    res = cbind(data[,1], res)
+    names(res)=c("Genotype",paste(unique(data[,2]),d2,sep="."))
+    
+    #store
+    outname = file.path(dirname(filename), gsub(".txt","_out.csv",basename(filename),) )
+    write.csv(res,outname, row.names=F)
+  }
+  res
+}
+
+
+
+
--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 licor
 =====
 
-Some small utility tools to process molecular marker data
+The package provides currently minimal functionality to convert licor data 
+to a more common data format of a matrix of 0 and 1. 
+
+The function identifies all unique base pair weights and sorts them ascending into columns.
+Genotypes are marked with 0 and 1 for absence or presence of the fragment.
+
+Very much work in progress.
+
+Installation
+============
+
+```{r}
+install.packages("devtools")
+devtools::install_github("licor", user="c5sire")
+```
+
+Usage
+======
+
+The function will ask using a file dialog for a data archive to process. The file format has no header. The columns are
+1. Name of genotype (or box)
+2. Name of marker
+3. and beyond: data columns
+
+```{r}
+library(licor)
+
+data = licor2matrix()
+```
+
